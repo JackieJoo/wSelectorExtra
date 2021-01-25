97528ab4aab434c09e8481a268f9ad074498db51
Jack Shell <jackshel45@gmail.com>
2021-01-25 16:42:41 +0200

    Rewrite sample

--- sample/trivial/Sample.s
+++ sample/trivial/Sample.s
@@ -3,10 +3,21 @@ let _ = require( 'wselectorextra' );
 
 /**/
 
-var src = { a : [ 1, 2, 3 ], b : { b1 : 'x' }, c : 'test' }
-var got = _.entityProbe({ src });
+let src = { a : { b : 1 }, c : { d : 2 } };
+let got = _.entityProbe({ src, recursive : 1 });
 
-console.log( got.report );
+console.log( got );
 /*
-Probe : 3
+{ src: { a: { b: 1 }, c: { d: 2 } },
+  recursive: 1,
+  result:
+   [Object: null prototype] {
+     b:
+      [Object: null prototype] { times: 1, values: [Array], having: [Array], notHaving: [Array] },
+     d:
+      [Object: null prototype] { times: 1, values: [Array], having: [Array], notHaving: [Array] } },
+  log: 'Probe : 2\n  *.b : 1 1\n  *.d : 1 2\n',
+  total: 2,
+  all: [ { b: 1 }, { d: 2 } ],
+  title: 'Probe' }
 */
